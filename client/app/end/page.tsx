1b5da9f42378b3acc9e362d5f86613538cfe169c
parth1007 <parthsoni03ps@gmail.com>
2023-11-04 22:49:43 +0530

    JD integration

--- client/app/end/page.tsx
+++ client/app/end/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Analysis from "./analysisCard";
 import { useRouter } from 'next/navigation'
 import { Button } from "@/components/ui/button";
@@ -52,6 +52,8 @@ const data : CardDataFormat[] = [
   },
 ]
 
+
+
 export default function Analyses() {
   
   const router = useRouter()
@@ -64,6 +66,37 @@ export default function Analyses() {
   const prevQst = () => {
     setqst((curQst) => curQst-1)
   }
+
+  useEffect(() => {
+    const getDataFromLocalStorage = () => {
+      const questionsData = localStorage.getItem('questionsData'); 
+      if (questionsData) {
+        const parsedData = JSON.parse(questionsData);
+  
+        const parsedList = parsedData.split('\n');
+  
+        // @ts-ignore
+        const questions = parsedList.map(line=> line.slice(line.indexOf(' ') + 1));
+  
+        // @ts-ignore
+        const filteredDescriptions = questions.filter(question => question.trim() !== '');
+  
+        console.log(filteredDescriptions)
+
+        // setqsts(parsedList);
+      }
+    };
+  
+    getDataFromLocalStorage();
+  }, []);
+
+
+  const deleteFromLocalStorage = (key : string) => {
+    if (localStorage.getItem(key) !== null){
+      localStorage.removeItem(key); 
+    }
+  };
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-slate-800 bg-[#f7f8fa] ">
@@ -82,7 +115,7 @@ export default function Analyses() {
         }
       </div>
       <div>
-        <Button variant={'outline'} className="bg-blue-50 hover:bg-blue-50 text-blue-600 hover:text-blue-700 text-md font-medium py-3 px-4">
+        <Button variant={'outline'} className="bg-blue-50 hover:bg-blue-50 text-blue-600 hover:text-blue-700 text-md font-medium py-3 px-4" onClick={() => {deleteFromLocalStorage("questionsData")}}>
           <RotateCcw className="h-6 w-6"></RotateCcw>
           <span className="pl-2">Practice Again</span>
         </Button>
--- client/app/session/JobDescDialog.tsx
+++ client/app/session/JobDescDialog.tsx
@@ -9,11 +9,58 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import axios from "axios";
+import { useState } from "react";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
 export function UploadJD() {
+
+  const [tags, setTags] = useState([])
+  const [jobDesc, setJobDesc] = useState("")
+
+
+  const makeAPICall = async () => {
+    const apiKey = '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'; 
+    const endpoint = 'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions'; 
+    
+    if(jobDesc === ""){
+      alert("Please enter Job description")
+      return;
+    }
+
+    try {
+      const input_prompt = {
+        prompt: `Job description - {jobDesc}
+        Extract the top 10 keywords for interview practice from the given job description. Don't include extra words above or below the list.` ,
+        max_tokens: 200
+      };
+
+      const config = {
+        headers: {
+          'Authorization': `Bearer ${apiKey}`,
+          "Content-type": "application/json",
+        },
+      };
+
+      const {data} = await axios.post(endpoint,input_prompt, config);
+      console.log(data);
+      
+    } catch (error) {
+        alert("Some error occured. Please try again");
+        console.log(error);
+    }
+}
+
+  //@ts-ignore
+  const handleInputChange = (event) => {
+    setJobDesc(event.target.value);
+    console.log(jobDesc);
+  };
+
+
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -34,14 +81,14 @@ export function UploadJD() {
           </DialogDescription>
         </DialogHeader>
         <div className="mt-4">
-            <Textarea className="text-sm"/>
+            <Textarea className="text-sm"  value={jobDesc} onChange={handleInputChange}/>
             <p className="text-sm text-muted-foreground">
                 Job Description will be used to find relevent tags for interview
             </p>
         </div>
         <DialogFooter className="mt-4">
             <DialogClose asChild>
-            <Button type="submit">Upload</Button>
+            <Button type="submit" onClick={makeAPICall}>Upload</Button>
             </DialogClose>
         </DialogFooter>
       </DialogContent>

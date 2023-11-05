df94e9771805ba0f29e14e9e1ed27ed2485f5815
Tarunsingh2802 <iec2020064@iiita.ac.in>
2023-11-05 07:59:07 +0530

    Added endpoint for final combined analysis and function to generate prompt for the same

--- server/elevate/urls.py
+++ server/elevate/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.start_interview, name='start_interview'),
     path('analyse/',views.start_analysis, name='start_analysis'),
+    path('final/',views.start_final_analysis, name='start_final_analysis'),
 ]
 
 
--- server/elevate/views.py
+++ server/elevate/views.py
@@ -9,7 +9,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-def generate_prompt(keywords, difficulty, prompt_type=None):
+def generate_questions_prompt(keywords, difficulty, prompt_type=None):
 
     if difficulty == "Beginner":
         prompt = f"Generate 5 interview questions based on any random topics from the following set of topics - {keywords}. The level of questions should be {difficulty}. Shuffle the questions. Don't include extra words above or after the questions."
@@ -24,10 +24,41 @@ def generate_analysis_prompt(question, answer):
     prompt = f"Interviewer\'s question -  {question} \nMy answer - {answer} \nGive a short and crisp analysis of my answer to the interviewer\'s question in not more than 100 words. The format of the structured analysis should be as follows- \nRelevance and Accuracy: \nDepth of knowledge: \nConfidence and Clarity: \nStrong topics: \nWeak topics: \nKeep the analysis to the point. Also rate my answer on a scale of 10 and give the rating at the end as \"Rating: \". Don\'t include extra words above or after the analysis."
     return prompt
 
-def send_request(keywords,difficulty):
+
+def generate_final_analysis_prompt(combined_analysis):
+
+    analysis_list=combined_analysis.split('<SEP>')
+    processed_text=""
+
+    cnt=1
+    for analysis in analysis_list:
+        analysis=analysis.strip()
+        if analysis!='': 
+            processed_text = processed_text + "Analysis-" + str(cnt) + ":\n" + analysis + "\n"
+            cnt+=1
+    
+    prompt = f"I had an interview in which I was asked various questions. Here is the list of analyses of each of my interview answers. Analyse and give insights about my performance in the interview - \n{processed_text} \nGive a brief overall combined analysis using the given individual analysis of each of my answer in the interview. Keep the analysis of not more than 250 words. Keep the analysis to the point. Don\'t include extra words above or after the analysis. "
+    
+    return prompt
+
+
+def modify_text(text): 
+
+    topics_list=text.split('\n')
+    modified_text={}
+
+    for topic in topics_list:
+        topic=topic.strip()
+        if topic!='': 
+            key,value=topic.split(": ")
+            modified_text[key]=value
+
+    return modified_text
+
+def send_questions_request(keywords,difficulty):
 
     try:
-        prompt = generate_prompt(keywords,difficulty)
+        prompt = generate_questions_prompt(keywords,difficulty)
         
         if prompt:
             openai.api_key = OPENAI_API_KEY
@@ -45,19 +76,6 @@ def send_request(keywords,difficulty):
 
     except:
         raise PromptError("OpenAI API error")
-
-def modify_text(text): 
-
-    topics_list=text.split('\n')
-    modified_text={}
-    
-    for topic in topics_list:
-        topic=topic.strip()
-        if topic!='': 
-            key,value=topic.split(": ")
-            modified_text[key]=value
-
-    return modified_text
 
 
 def send_analysis_request(question,answer):
@@ -85,6 +103,33 @@ def send_analysis_request(question,answer):
     except:
         raise PromptError("OpenAI API error")
 
+
+def send_final_analysis_request(combined_analysis):
+
+    try:
+        prompt = generate_final_analysis_prompt(combined_analysis)
+        
+        if prompt:
+            openai.api_key = OPENAI_API_KEY
+
+            response = openai.Completion.create(
+                model="gpt-3.5-turbo-instruct",
+                prompt=prompt,
+                max_tokens=1000
+            )
+            generated_text = response.choices[0].text  
+            print(prompt)
+            print("--------------")
+            print(generated_text)
+            return generated_text
+        
+        else:
+            raise PromptError("Prompt is empty")
+
+    except:
+        raise PromptError("OpenAI API error")
+
+
 @csrf_exempt 
 def start_interview(request):
 
@@ -95,7 +140,7 @@ def start_interview(request):
             difficulty = data['difficulty']
 
             
-            response = send_request(keywords,difficulty)
+            response = send_questions_request(keywords,difficulty)
 
             if(len(keywords) == 0):
                 return HttpResponse("Invalid Input", status=404)
@@ -120,6 +165,7 @@ def start_interview(request):
         error_message = f"An error occurred: {str(e)}"
         return HttpResponse(error_message, status=500)
     
+
 @csrf_exempt 
 def start_analysis(request):
 
@@ -133,6 +179,40 @@ def start_analysis(request):
             response = send_analysis_request(question,answer)
 
             if(question == "" or answer == ""):
+                return HttpResponse("Invalid Input", status=404)
+            
+            return HttpResponse(response)
+
+        else:
+            return HttpResponse("Bad request type", status=400)
+        
+
+
+    # Exceptions
+
+    except PromptError as e:
+        error_message = f"An error occurred: {str(e)}"
+        return HttpResponse(error_message, status=500)
+    
+    except ObjectDoesNotExist:
+        return HttpResponse("Object does not exist.", status=404)
+    
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return HttpResponse(error_message, status=500)
+
+
+@csrf_exempt 
+def start_final_analysis(request):
+
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.body.decode('utf-8'))
+            combined_analysis = data['combined_analysis']
+            
+            response = send_final_analysis_request(combined_analysis)
+
+            if(combined_analysis == ""):
                 return HttpResponse("Invalid Input", status=404)
             
             return HttpResponse(response)
